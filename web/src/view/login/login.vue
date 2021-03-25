79add1f8e3a531f9ab814265372b7d7688595ebc
unknown <1251424057@qq.com>
2021-03-25 16:12:06 +0800

    '修改初始用户'

--- web/src/view/login/login.vue
+++ web/src/view/login/login.vue
@@ -108,7 +108,7 @@ export default {
       curYear: 0,
       lock: "lock",
       loginForm: {
-        username: "admin",
+        username: "init",
         password: "123456",
         captcha: "",
         captchaId: "",
