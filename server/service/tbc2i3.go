ae05a867b36ea93df1f59560aa10513ae5993bc8
unknown <1251424057@qq.com>
2021-04-19 10:56:27 +0800

    '去除debug语句‘

--- server/service/tbc2i3.go
+++ server/service/tbc2i3.go
@@ -3,5 +3,5 @@ package service
 import "gin-vue-admin/global"
 
 func SectorTripleAnalysis(threshold float64) error {
-	return global.GVA_DB.Debug().Exec("call triple_analysis(?)", threshold).Error
+	return global.GVA_DB..Exec("call triple_analysis(?)", threshold).Error
 }
